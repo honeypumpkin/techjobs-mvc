72df001386d3452b2de35b18ebfdea4219c61a53
honeypumpkin <robin.avila88@gmail.com>
2017-07-21 07:37:56 -0500

    Search request and display handler

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -16,10 +17,20 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
-    public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
-        return "search";
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String returnUserSearch(Model model,
+                                   @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }else {
+            model.addAttribute("columns", ListController.columnChoices);
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
     }
 
     // TODO #1 - Create handler to process search request and display results
