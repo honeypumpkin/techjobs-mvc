f6f6c29a2875c54c27c798fd8ae4ceef41cfdea3
honeypumpkin <robin.avila88@gmail.com>
2017-07-21 08:30:32 -0500

    Complete TJMVC

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import jdk.nashorn.internal.scripts.JO;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,22 +19,27 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "results", method = RequestMethod.GET)
-    public String returnUserSearch(Model model,
-                                   @RequestParam String searchType, @RequestParam String searchTerm) {
-
-        if (searchType.equals("all")) {
-            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
-            model.addAttribute("jobs", jobs);
-            return "search";
-        }else {
-            model.addAttribute("columns", ListController.columnChoices);
-            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-            model.addAttribute("jobs", jobs);
-            return "search";
-        }
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String processJobSearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 
 }
